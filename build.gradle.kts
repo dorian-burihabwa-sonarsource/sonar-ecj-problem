ccbf82e09d67570bcea10ebf9e33f129e6d578c7
Martin Rouček <martin.roucek@rapidaddition.com>
2020-03-26 19:46:21 +0100

    New sonar version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 
 dependencies {
     "compile"(group = "net.openhft", name = "affinity", version = "3.2.2")
+    "compile"("com.google.inject:guice:4.2.2")
 }
--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,10 +1,16 @@
 package example;
 
+import com.google.common.io.Files;
 import net.openhft.affinity.AffinityLock;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public int plus() {
+
+    public int plus() throws IOException {
         AffinityLock affinityLock = AffinityLock.acquireCore();
+        Files.createParentDirs(new File("SomeFile"));
         String problematicString = "SomeString";
         System.out.println(problematicString);
         return 2;
